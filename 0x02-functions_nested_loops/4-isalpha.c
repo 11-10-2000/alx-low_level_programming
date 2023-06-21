51b04dc56cc3d226f95d42caaa7774d67276b774
11-10-2000 <phenyoserepa@gmail.com>
2023-06-20 17:44:37 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for Alphabetic character.
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for letters. 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+        _putchar('\n');
+}
+
